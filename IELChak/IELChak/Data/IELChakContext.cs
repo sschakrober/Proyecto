ab3f635742188bae26c8b5b0805355f5374bfa4a
chakrober <sschakrober@gmail.com>
2018-11-04 00:07:52 -0300

    commit1

---
+++ IELChak/IELChak/Data/IELChakContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace IELChak.Models
+{
+    public class IELChakContext : DbContext
+    {
+        public IELChakContext (DbContextOptions<IELChakContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<IELChak.Models.Categoria> Categoria { get; set; }
+    }
+}
---
+++ IELChak/IELChak/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IELChak.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="El nombre debe tener de 3 a 50 caracteres")]
+        public string Nombre { get; set; }
+        [StringLength(256, ErrorMessage ="La descripcion no debe exceder los 256 caracteres")]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
