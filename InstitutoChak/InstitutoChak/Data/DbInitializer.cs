30eb89e9ec540b6599bcb4e7c08db14ec5dcf4be
chakrober <sschakrober@gmail.com>
2018-11-06 16:09:27 -0300

    commit3

---
+++ InstitutoChak/InstitutoChak/Data/DbInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InstitutoChak.Models;
+
+namespace InstitutoChak.Data
+{
+    public class DbInitializer
+    {
+        public static void Initializer(InstitutoChakContext context)
+        {
+            context.Database.EnsureCreated();
+            if (context.Categoria.Any())
+            {
+                return;
+            }
+
+            var categorias = new Categoria[]
+            {
+                new Categoria {Nombre="Programacion", Descripcion="Curso de Programacion", Estado = true},
+                new Categoria {Nombre="Diseño Grafico", Descripcion="Curso de Diseño Grafico", Estado = true},
+                new Categoria {Nombre="Juegos 3D", Descripcion="Curso de Plataforma de Video Juegos", Estado = true},
+                new Categoria {Nombre="Office", Descripcion="Paquete Office", Estado = true}
+            };
+
+            foreach (Categoria c in categorias)
+            {
+                context.Categoria.Add(c);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ InstitutoChak/InstitutoChak/Data/InstitutoChakContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace InstitutoChak.Models
+{
+    public class InstitutoChakContext : DbContext
+    {
+        public InstitutoChakContext (DbContextOptions<InstitutoChakContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<InstitutoChak.Models.Categoria> Categoria { get; set; }
+    }
+}
